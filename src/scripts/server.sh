9e73a50dcb28b803a0a9411fd6ec1023a6292157
Jon Hinks <projects.ws@jondh.me.uk>
2016-09-18 15:29:22 +0100

    Add server start script

---
+++ src/scripts/server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Save pwd and then change dir to the script location
+STARTDIR=`pwd`
+cd `dirname $0`/../../..
+
+# Start up built-in web server
+php \
+	-S 127.0.0.1:8090 \
+	-t web \
+	test/browser/scripts/router.php \
+	2> /dev/null
+
+# Go back to original dir
+cd $STARTDIR
