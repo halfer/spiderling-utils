94b319f966456641c2bbc1b0b7a393e724587b42
Jon Hinks <projects.ws@jondh.me.uk>
2016-09-18 15:41:28 +0100

    Temporary fix to router pathname

--- src/scripts/server.sh
+++ src/scripts/server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# @todo Need to pass router script path in as an argument
 
 # Save pwd and then change dir to the script location
 STARTDIR=`pwd`
@@ -8,7 +10,7 @@ cd `dirname $0`/../../..
 php \
 	-S 127.0.0.1:8090 \
 	-t web \
-	test/browser/scripts/router.php \
+	../../test/browser/scripts/router.php \
 	2> /dev/null
 
 # Go back to original dir
