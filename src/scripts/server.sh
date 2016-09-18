3c5c35059737f291a1c20ca06ca69c4be2986ff9
Jon Hinks <projects.ws@jondh.me.uk>
2016-09-18 15:58:26 +0100

    Temporary fix on docroot path

--- src/scripts/server.sh
+++ src/scripts/server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # @todo Need to pass router script path in as an argument
+# @todo Need to pass web root path in as an argument
 
 # Save pwd and then change dir to the script location
 STARTDIR=`pwd`
@@ -9,7 +10,7 @@ cd `dirname $0`/../../..
 # Start up built-in web server
 php \
 	-S 127.0.0.1:8090 \
-	-t web \
+	-t ../../web \
 	../../test/browser/scripts/router.php \
 	2> /dev/null
 
