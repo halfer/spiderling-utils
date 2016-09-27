f114f5e2034e510c54a0ac7bca1196b12dbbbf84
Jon Hinks <projects.ws@jondh.me.uk>
2016-09-27 20:21:30 +0100

    The web server should be run in the background

--- src/scripts/server.sh
+++ src/scripts/server.sh
@@ -16,10 +16,10 @@ cd `dirname $0`/../..
 
 if [ "$4" = "" ]; then
 	# Start up built-in web server without router script
-	php -S $1 -t $2      2> /dev/null
+	php -S $1 -t $2      2> /dev/null &
 else
 	# Start up built-in web server with router script
-	php -S $1 -t $2 $4   2> /dev/null
+	php -S $1 -t $2 $4   2> /dev/null &
 fi;
 
 # Go back to original dir
