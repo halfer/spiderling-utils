ff8b09fea9937b26590d3c896ee7d72b926fa660
Jon Hinks <projects.ws@jondh.me.uk>
2016-09-28 21:00:59 +0100

    Support multiple pages and 404 responses in the router script

--- test/browser/scripts/router.php
+++ test/browser/scripts/router.php
@@ -20,5 +20,30 @@ elseif ($_SERVER["REQUEST_URI"] == '/server-check')
 else
 {
 	// Send page requests to the web app
-	include $root . "/test/browser/docroot/index.php";
+	$docRoot = $root . "/test/browser/docroot";
+	$page = null;
+	if ($_SERVER["REQUEST_URI"] == '/')
+	{
+		$page = $docRoot . "/index.php";
+	}
+	else
+	{
+		// @todo Ensure we have no .. naughtiness in our URL
+		$possiblePath = $docRoot . $_SERVER["REQUEST_URI"];
+		if (file_exists($possiblePath))
+		{
+			$page = $possiblePath;
+		}
+	}
+
+	if ($page)
+	{
+		include $page;
+	}
+	else
+	{
+		// Send back 404 page/header
+		header("HTTP/1.0 404 Not Found");
+		echo "Not found\n";
+	}
 }
