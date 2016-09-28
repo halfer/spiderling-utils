28f9f4cf8d3aa3c3ded0815abc56b53d178e4cf3
Jon Hinks <projects.ws@jondh.me.uk>
2016-09-28 19:19:25 +0100

    Add skeleton for new functional tests

---
+++ test/browser/tests/LoggingTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace halfer\SpiderlingUtils\Test;
+
+/**
+ * Tests to show the PhantomJS logging options are working correctly
+ */
+
+class LoggingTest extends TestCase
+{
+	public function testLoggingIsWorking()
+	{
+		// FIXME
+	}
+
+	public function testNotLoggingIsWorking()
+	{
+		// FIXME
+	}
+}
---
+++ test/browser/tests/ScreenshotTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace halfer\SpiderlingUtils\Test;
+
+/**
+ * Tests to show the screenshot system in PhantomJS is working correctly
+ */
+class ScreenshotTest extends TestCase
+{
+	public function testScreenshot()
+	{
+		// FIXME
+	}
+}
