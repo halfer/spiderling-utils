0f5625caf97cae83c941b09b35037accf5521133
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-11 20:56:27 +0100

    Exit with failure code if either set of tests fails

--- test/test.sh
+++ test/test.sh
@@ -5,14 +5,25 @@
 # Run the standard tests
 ${PROJECTROOT}/vendor/bin/phpunit
 
+# If there were errors then exit with failure
+if [[ $? -ne 0 ]]; then
+	exit 1
+fi
+
 # Now for the v6 tests
 VERSION=`phpenv version-name`
 SUBROOT=${PROJECTROOT}/test/phpunit
 
 # Version 6 of PHPUnit is just for PHP 7.0+
 if ([ "$VERSION" == "7.0" ] || [ "$VERSION" == "7.1" ]); then
+
 	echo "PHP 7.x, running extra tests"
 	${SUBROOT}/v6/vendor/bin/phpunit --configuration ${SUBROOT}/v6/
+
+	# If there were errors then exit with failure
+	if [[ $? -ne 0 ]]; then
+		exit 1
+	fi
 else
 	echo "Not PHP 7.x, nothing extra to test"
 fi
