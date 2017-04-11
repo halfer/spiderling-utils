38391608dfa07be5a5831be1a5ecd30fa1690c83
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-11 20:01:17 +0100

    More verbose output from new installation/test phases

--- test/phpunit/install.sh
+++ test/phpunit/install.sh
@@ -6,5 +6,8 @@ VERSION=`phpenv version-name`
 
 # Version 6 of PHPUnit is just for PHP 7.0+
 if ([ "$VERSION" == "7.0" ] || [ "$VERSION" == "7.1" ]); then
+	echo "PHP 7.x, installing extra dependencies"
 	composer install --working-dir ${PROJECTROOT}/test/phpunit/v6
+else
+	echo "Not PHP 7.x, nothing extra to install"
 fi
--- test/phpunit/test.sh
+++ test/phpunit/test.sh
@@ -8,5 +8,8 @@ SUBROOT=${PROJECTROOT}/test/phpunit
 
 # Version 6 of PHPUnit is just for PHP 7.0+
 if ([ "$VERSION" == "7.0" ] || [ "$VERSION" == "7.1" ]); then
+	echo "PHP 7.x, running extra tests"
 	${SUBROOT}/v6/vendor/bin/phpunit --configuration ${SUBROOT}/v6/
+else
+	echo "Not PHP 7.x, nothing extra to test"
 fi
