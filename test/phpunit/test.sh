c7fa4882920ba349a91cd1ec8ad83bf676aead14
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-11 19:21:14 +0100

    Add configuration switch to PHPUnit call

--- test/phpunit/test.sh
+++ test/phpunit/test.sh
@@ -4,7 +4,9 @@
 
 VERSION=`phpenv version-name`
 
+SUBROOT=${PROJECTROOT}/test/phpunit
+
 # Version 6 of PHPUnit is just for PHP 7.0+
 if ([ "$VERSION" == "7.0" ] || [ "$VERSION" == "7.1" ]); then
-	${PROJECTROOT}/test/phpunit/v6/vendor/bin/phpunit
+	${SUBROOT}/v6/vendor/bin/phpunit --configuration ${SUBROOT}/v6/
 fi
