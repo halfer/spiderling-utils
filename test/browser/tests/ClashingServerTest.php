9f903c7537c4d0ecd19cc1b1c4cd9f796305d289
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-11 06:14:28 +0100

    Repeatedly try this test until two servers have tried to start
    (Attempt to reduce flakiness on Travis, which is probably slow)

--- test/browser/tests/ClashingServerTest.php
+++ test/browser/tests/ClashingServerTest.php
@@ -8,28 +8,49 @@ namespace halfer\SpiderlingUtils\Test;
 class ClashingServerTest extends TestCase
 {
 	/**
-	 * @todo Maybe replace the sleep() with a file-based check to ensure both servers have had
-	 * a chance to start up?
+	 * Uses a file-based check to ensure both servers have had a chance to start
 	 */
 	public function testClashingServers()
 	{
-		// Need to wait for servers to try to start up first
-		sleep(2);
 
-		$failedCount = 0;
-		for($suffix = 1; $suffix <= 2; $suffix++)
+		$startTime = microtime(true);
+		do
 		{
-			$path = "/tmp/spiderling-utils-clash-{$suffix}.pid";
-			if (file_exists($path))
+			$loggedCount = 0;
+			$failedCount = 0;
+			for($suffix = 1; $suffix <= 2; $suffix++)
 			{
-				$error = file_get_contents($path);
-				if (strpos($error, 'Address already in use') !== false)
+				$path = "/tmp/spiderling-utils-clash-{$suffix}.pid";
+				if (file_exists($path))
 				{
-					$failedCount++;
+					$loggedCount++;
+					$error = file_get_contents($path);
+					if (strpos($error, 'Address already in use') !== false)
+					{
+						$failedCount++;
+					}
 				}
 			}
-		}
+		} while ($loggedCount < 2 && $this->sleepWithLimit($startTime));
 
 		$this->assertEquals(1, $failedCount, "Check that one clashing server failed");
+	}
+
+	/**
+	 * Helps waits for up to five seconds before giving up
+	 *
+	 * @param float $startTime
+	 * @return boolean
+	 */
+	protected function sleepWithLimit($startTime)
+	{
+		$timeNow = microtime(true);
+		$carryOn = $timeNow - $startTime < 5;
+		if ($carryOn)
+		{
+			usleep(100000);
+		}
+
+		return $carryOn;
 	}
 }
