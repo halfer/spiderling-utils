15c378e48efe1af4f5c14d475c6b413b4c3e00bc
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-12 11:41:07 +0100

    Swap manual loading for autoloader

--- test/phpunit/install.sh
+++ test/phpunit/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Script to do installs depending on PHP version running
+#
+# Note that PhantomJS is not being installed, we cheat and use the copy in the parent vendor/bin!
 
 VERSION=`phpenv version-name`
 
--- test/phpunit/v6/test/bootstrap.php
+++ test/phpunit/v6/test/bootstrap.php
@@ -10,12 +10,21 @@ require_once $projectRoot . '/vendor/autoload.php';
 
 // Loads the parent project as if it were a Composer dependency pointing to dev-master
 $parentRoot = realpath($projectRoot . '/../../..');
-require_once $parentRoot . '/src/library/SpiderlingUtils/Server.php';
-require_once $parentRoot . '/src/library/SpiderlingUtils/Feature/TestCase.php';
-require_once $parentRoot . '/src/library/SpiderlingUtils/Feature/TestListener.php';
-require_once $parentRoot . '/src/library/SpiderlingUtils/NamespacedTestCase.php';
-require_once $parentRoot . '/src/library/SpiderlingUtils/NamespacedTestListener.php';
-require_once $parentRoot . '/src/library/SpiderlingUtils/Driver_Phantomjs.php';
+
+// I should be able to use the autoloader in Composer, but that didn't seem to work,
+// so using a little custom autoloader for now.
+spl_autoload_register(
+	function($class) use ($parentRoot)
+	{
+		if (strpos($class, 'halfer\\SpiderlingUtils\\') !== false)
+		{
+			$removeNamespace = str_replace('halfer\\', '', $class);
+			$path = str_replace('\\', '/', $removeNamespace);
+			$fullPath = $parentRoot . "/src/library/{$path}.php";
+			require_once $fullPath;
+		}
+	}
+);
 
 // Load test classes
 require_once $projectRoot . '/classes/TestListener.php';
